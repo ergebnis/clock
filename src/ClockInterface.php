1ac392473f2c9da404be9d7b12fa6ebe7c6573fc
Andreas Möller <am@localheinz.com>
2018-01-30 23:46:30 +0100

    Enhancement: Implement SystemClock

---
+++ src/ClockInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/clock
+ */
+
+namespace Localheinz\Clock;
+
+interface ClockInterface
+{
+    public function now(): \DateTimeImmutable;
+}
---
+++ test/Unit/SystemClockTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/clock
+ */
+
+namespace Localheinz\Clock\Test\Unit;
+
+use Localheinz\Clock\ClockInterface;
+use Localheinz\Clock\SystemClock;
+use Localheinz\Test\Util\Helper;
+use PHPUnit\Framework;
+
+final class SystemClockTest extends Framework\TestCase
+{
+    use Helper;
+
+    /**
+     * @var string
+     */
+    private $dateTimeDefault;
+
+    protected function setUp()
+    {
+        $this->dateTimeDefault = \date_default_timezone_get();
+    }
+
+    protected function tearDown()
+    {
+        \date_default_timezone_set($this->dateTimeDefault);
+    }
+
+    public function testImplementsClockInterface()
+    {
+        $this->assertClassImplementsInterface(ClockInterface::class, SystemClock::class);
+    }
+
+    public function testNowReturnsCurrentDateTime()
+    {
+        $clock = new SystemClock();
+
+        $before = new \DateTimeImmutable();
+
+        $now = $clock->now();
+
+        $after = new \DateTimeImmutable();
+
+        $this->assertGreaterThanOrEqual($before, $now);
+        $this->assertLessThanOrEqual($after, $now);
+    }
+
+    public function testNowReturnsCurrentDateTimeWithCurrentTimezone()
+    {
+        $clock = new SystemClock();
+
+        $now = $clock->now();
+
+        $this->assertSame(\date_default_timezone_get(), $now->getTimezone()->getName());
+    }
+
+    public function testNowReturnsCurrentDateTimeWithInitializedTimezone()
+    {
+        $timezone = new \DateTimeZone('Europe/Berlin');
+
+        $clock = new SystemClock($timezone);
+
+        $now = $clock->now();
+
+        $this->assertSame($timezone->getName(), $now->getTimezone()->getName());
+    }
+}
